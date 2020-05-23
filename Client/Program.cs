fd0fc9e355f8b93dfb678cacc75e25abbb99e07d
HRida <H.Rida1@hotmail.com>
2020-05-24 00:34:38 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listen = new TcpListener(3003);
+            TcpClient client;
+            int bufferSize = 1024;
+            NetworkStream netStream;
+            int bytesRead = 0;
+            int allBytesRead = 0;
+
+            // Start listening
+            listen.Start();
+
+            // Accept client
+            client = listen.AcceptTcpClient();
+            netStream = client.GetStream();
+
+            // Read length of incoming data
+            byte[] length = new byte[4];
+            bytesRead = netStream.Read(length, 0, 4);
+            int dataLength = BitConverter.ToInt32(length, 0);
+
+            // Read the data
+            int bytesLeft = dataLength - 4;
+            byte[] data = new byte[dataLength];
+
+            while (bytesLeft > 0)
+            {
+
+                int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+
+                bytesRead = netStream.Read(data, allBytesRead, nextPacketSize);
+                allBytesRead += bytesRead;
+                bytesLeft -= bytesRead;
+
+            }
+
+            // Save image to desktop
+            File.WriteAllBytes("H:\\x2.jpg", data);
+
+            // Clean up
+            netStream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 3003;
+            int bufferSize = 1024;
+
+            TcpClient client = new TcpClient();
+            NetworkStream netStream;
+
+            // Connect to server
+            try
+            {
+                client.Connect(new IPEndPoint(ipAddress, port));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            netStream = client.GetStream();
+
+            // Read bytes from image
+            byte[] data = File.ReadAllBytes("H:\\x.jpg");
+
+            // Build the package
+            byte[] dataLength = BitConverter.GetBytes(data.Length);
+            byte[] package = new byte[4 + data.Length];
+            dataLength.CopyTo(package, 0);
+            data.CopyTo(package, 4);
+
+            // Send to server
+            int bytesSent = 0;
+            int bytesLeft = package.Length;
+
+            while (bytesLeft > 0)
+            {
+
+                int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+
+                netStream.Write(package, bytesSent, nextPacketSize);
+                bytesSent += nextPacketSize;
+                bytesLeft -= nextPacketSize;
+
+            }
+
+            // Clean up
+            netStream.Close();
+            client.Close();
+        }
+    }
+}
